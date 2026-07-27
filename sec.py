cb2352f8fba9b2c1a735f1c6d4815624230bc2ae
sabhay02 <sabhaythakkar02@gmail.com>
2026-07-27 20:44:05 +0530

    Add user authentication and utility functions

---
+++ sec.py
@@ -0,0 +1,42 @@
+import os
+import sqlite3
+import hashlib
+
+SECRET_KEY = "my-super-secret-key"
+
+
+def login(username, password):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    query = (
+        "SELECT * FROM users WHERE username='"
+        + username
+        + "' AND password='"
+        + password
+        + "'"
+    )
+
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
+
+
+def read_file(path):
+    f = open(path, "r")
+    data = f.read()
+    return data
+
+
+def execute_command(cmd):
+    os.system(cmd)
+
+
+def calculate_discount(price, discount):
+    return price - (price * discount / 100)
+
+
+unused_variable = 42
